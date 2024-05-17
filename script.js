c6ce3619e46cdb20586df0a761522444dc6eba38
Anupam <anupamprakash82@gmail.com>
2024-05-17 22:35:19 +0530

    style: manipulated the styles using JS

--- script.js
+++ script.js
@@ -27,10 +27,19 @@ document.querySelector('.check').addEventListener('click', function () {
     console.log('nothing is chosen');
     document.querySelector('.message').textContent = 'No number is entered';
   } else if (guess === secretNumber) {
+    //player wins
     console.log('you guesses the right number');
     document.querySelector('.message').textContent =
       'you have guessed the right number!!🥳';
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.check').disabled = true;
+
+    //manipulate the css styles
+    //1. change the background color
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    //2. widen the ? square to rectangle
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Your guess is high📈';
