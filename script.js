15714c65ada6053e3cb77788f7ccebc37129ca27
Anupam <anupamprakash82@gmail.com>
2024-05-17 22:13:46 +0530

    feat: implemented the game logic

--- script.js
+++ script.js
@@ -2,11 +2,20 @@
 /*
 console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'correct number!!🥳';
+document.querySelector('.message').textContent = 'correct secretNumber!!🥳';
 
 // document.querySelector('.guess').value = 23;
-// console.log(guessedNumber);
+// console.log(guessedsecretsecretNumber);
 */
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+console.log(secretNumber);
+
+let score = 20;
+
+// let currentScore = document.querySelector('.score').value;
+// console.log(currentScore);
+// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   //   console.log('button checked clicked!!!');/
@@ -17,5 +26,30 @@ document.querySelector('.check').addEventListener('click', function () {
   if (!guess) {
     console.log('nothing is chosen');
     document.querySelector('.message').textContent = 'No number is entered';
+  } else if (guess === secretNumber) {
+    console.log('you guesses the right number');
+    document.querySelector('.message').textContent =
+      'you have guessed the right number!!🥳';
+    document.querySelector('.check').disabled = true;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Your guess is high📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game😔';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+  //guess < secretNumber
+  else {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Your guess is low📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game😔';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
